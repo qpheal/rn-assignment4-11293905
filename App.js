e16d1b6929533ef91ce3e1b8927d24e5760466f3
Adjei Rapheal <radjei119@st.ug.edu.gh>
2024-06-18 10:18:19 +0000

    Added LoginScreen

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React, { Component } from "react";
+import { AntDesign, Feather } from "@expo/vector-icons";
+export class App extends Component {
+  render() {
+    return (
+      <SafeAreaView
+        style={{ flex: 1, justifyContent: "center", marginHorizontal: "8%" }}
+      >
+        <View style={{ gap: 10 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItem: "center",
+            }}
+          >
+            <View>
+              <Text style={{ fontSize: 24 }}>Eric Appah</Text>
+              <Text
+                style={{ fontSize: 20, fontWeight: "400", color: "#95969D" }}
+              >
+                ericappah@gmail.com
+              </Text>
+            </View>
+            <View>
+              <Image
+                source={require("./assets/profile.jpg")}
+                style={{ height: 50, width: 50, borderRadius: 50 }}
+              />
+            </View>
+          </View>
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+          <View style={{ flexDirection: "row", gap: 10 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                backgroundColor: "#F2F2F3",
+                padding: 10,
+                gap: 7,
+                width: "80%",
+                borderRadius: 10,
+              }}
+            >
+              <AntDesign name="search1" size={24} color="black" />
+              <TextInput placeholder="Search a job or position " />
+            </View>
+            <View
+              style={{
+                backgroundColor: "#F2F2F3",
+                padding: 10,
+                borderRadius: 10,
+              }}
+            >
+              <TouchableOpacity>
+                <Image source={require("./assets/filter.5.png")} />
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginHorizontal: 10,
+            }}
+          >
+            <Text>Featured Jobs</Text>
+            <Text>All</Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ screens/LoginScreen.js
@@ -0,0 +1,134 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView
+      style={{ flex: 1, justifyContent: "center", marginHorizontal: "8%" }}
+    >
+      <View style={{ gap: 50 }}>
+        <View>
+          <Text
+            style={{
+              fontSize: 22,
+              fontWeight: "600",
+              color: "#356899",
+              marginBottom: 15,
+            }}
+          >
+            Jobizz
+          </Text>
+          <Text
+            style={{
+              fontSize: 22,
+              fontWeight: "600",
+              fontSize: 24,
+              marginBottom: 5,
+            }}
+          >
+            Welcome back 👋
+          </Text>
+          <Text style={{ fontSize: 22, fontWeight: "400", fontSize: 14 }}>
+            Let's log in. Apply for jobs!!
+          </Text>
+        </View>
+
+        <View style={{ gap: 15 }}>
+          <TextInput
+            placeholder="Name"
+            style={{
+              borderWidth: 1,
+              paddingHorizontal: 10,
+              paddingVertical: 7,
+              borderRadius: 10,
+              borderBlockColor: "#AFB0B6",
+              borderColor: "#afb0b6",
+            }}
+          />
+          <TextInput
+            placeholder="Email"
+            style={{
+              borderWidth: 1,
+              paddingHorizontal: 10,
+              paddingVertical: 7,
+              borderRadius: 10,
+              borderBlockColor: "#AFB0B6",
+              borderColor: "#afb0b6",
+            }}
+          />
+
+          <Button
+            title="Login"
+            style={{
+              backgroundColor: "#356899",
+              borderRadius: 25,
+              padding: 40,
+            }}
+          />
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            marginVertical: -10,
+          }}
+        >
+          <View style={{ flex: 1, height: 1, backgroundColor: "#ddd" }}></View>
+
+          <Text style={{ marginHorizontal: 10, color: "#aaa" }}>
+            Or continue with
+          </Text>
+
+          <View style={{ flex: 1, height: 1, backgroundColor: "#ddd" }}></View>
+        </View>
+
+        <View style={{ flexDirection: "row", justifyContent: "center", marginTop: -5}}>
+          <TouchableOpacity>
+            <Image source={require("./assets/Group 56.png")} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={require("./assets/Group 55.png")} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={require("./assets/Group 54.png")} />
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={{flexDirection: 'row',
+            justifyContent: "center",
+            alignContent: "center",
+            alignItems: "center",
+            marginTop: -10
+          }}
+        >
+          <Text style={{ textAlign: "center", color: "#BDBEC2" }}>
+            Haven't an account?  
+          </Text>
+          <TouchableOpacity>
+              <Text style={{ color: "#0000ff" }}> Register</Text>
+            </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
